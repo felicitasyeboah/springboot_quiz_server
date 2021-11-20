9fdeb5dc87b684d0e5d6ff229013153668bf41fd
306759 <christian.kobialka@stud.th-luebeck.de>
2021-11-21 00:56:42 +0100

    Attribute umbenannt für bessere Lesbarkeit in der Datenbank

--- fill_tables.sql
+++ fill_tables.sql
@@ -12,19 +12,19 @@ INSERT INTO PaF_GruppeB.category (category_name)
         ('Essen & Trinken'),
         ('Sport');
         
-INSERT INTO PaF_GruppeB.question (categoryid, question_text, answer_correct, answer_wrong1, answer_wrong2, answer_wrong3)
+INSERT INTO PaF_GruppeB.question (category_id, question_text, answer_correct, answer_wrong1, answer_wrong2, answer_wrong3)
 	VALUES
 		(4, 'Einen Feinschmecker nennt man auch?', 'Gourmet', 'Genießer', 'Gourmed', 'Leckermäulchen'),
         (1, 'Welches Metall leitet Wärme am besten?', 'Silber', 'Kupfer', 'Gold', 'Aluminium'),
         (5, 'Wie lange geht ein Marathon?', '42,195 Kilometer', '10 Kilometer', '25 Kilometer', '1000 Meter');
         
-INSERT INTO PaF_GruppeB.played_games (user1id, user2id, user1score, user2score)
+INSERT INTO PaF_GruppeB.played_games (user_id1, user_id2, user_score1, user_score2)
 	VALUES
 		(1, 2, 5, 2),
         (1, 3, 3, 2),
         (2, 3, 1, 4);
         
-INSERT INTO PaF_GruppeB.highscore_global (highscoreid, gameid, userid)
+INSERT INTO PaF_GruppeB.highscore_global (highscore_id, game_id, user_id)
 	VALUES
 		(1, 1, 1),
         (2, 3, 3),
