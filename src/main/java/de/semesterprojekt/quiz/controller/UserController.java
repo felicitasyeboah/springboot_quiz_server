c348ca749c4db3911aa08c27613269349148c458
Christian <christian.kobialka@stud.th-luebeck.de>
2021-11-28 14:05:56 +0100

    added methods to get only online users and online usernames

--- src/main/java/de/semesterprojekt/quiz/controller/UserController.java
+++ src/main/java/de/semesterprojekt/quiz/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,13 +19,56 @@ public class UserController {
 
     private UserRepository userRepository;
 
+    /**
+     * Constructor
+     * @param userRepository
+     */
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    //Returns a list of all Users
+    /**
+     * Returns a list of all users
+     * @return All users
+     */
     @GetMapping("")
     public List<User> Index(){
+
         return userRepository.findAll();
+    }
+
+    /**
+     * Returns a list of all online users
+     * @return List of online users
+     */
+    @GetMapping("online")
+    public List<User> getOnlineUsers(){
+
+        List<User> userOnlineList = new ArrayList<>();
+
+        //Extracts all online users
+        for(User user : Index()) {
+            if(user.isOnline() == true) {
+                userOnlineList.add(user);
+            }
+        }
+
+        return userOnlineList;
+    }
+
+    /**
+     * Returns a list of the online usernames
+     * @return List of online usernames
+     */
+    @GetMapping("onlineUsername")
+    public List<String> getOnlineUsernames(){
+
+        List<String> usernameList = new ArrayList<>();
+
+        for(User user : getOnlineUsers()) {
+            usernameList.add(user.getUserName());
+        }
+
+        return usernameList;
     }
 }
