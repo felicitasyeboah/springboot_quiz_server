9049fce9d2f399ad70533b043a67754b2e193fde
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-09 11:54:06 +0100

    added a random question get controller

--- src/main/java/de/semesterprojekt/quiz/controller/QuestionController.java
+++ src/main/java/de/semesterprojekt/quiz/controller/QuestionController.java
@@ -4,9 +4,11 @@ import de.semesterprojekt.quiz.entity.Question;
 import de.semesterprojekt.quiz.repository.QuestionRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * The class controls the REST-mapping for the Question-entity
@@ -23,7 +25,19 @@ public class QuestionController {
 
     //Returns a list of all questions
     @GetMapping("")
-    public List<Question> Index(){
+    public List<Question> index(){
         return questionRepository.findAll();
+    }
+
+    @GetMapping("/random")
+    public Question getRandom() {
+        Random random = new Random();
+
+
+        List<Question> questionList = index();
+        int max = questionList.size();
+        int randomNumber = random.nextInt(max);
+
+        return questionList.get(randomNumber);
     }
 }
