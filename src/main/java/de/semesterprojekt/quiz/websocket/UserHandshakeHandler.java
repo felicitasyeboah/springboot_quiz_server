ca40177d3acc5f1b36754595fc061c54deaa02f5
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-22 23:29:34 +0100

    deleted unused sourcecode

--- src/main/java/de/semesterprojekt/quiz/websocket/UserHandshakeHandler.java
+++ src/main/java/de/semesterprojekt/quiz/websocket/UserHandshakeHandler.java
@@ -2,6 +2,8 @@ package de.semesterprojekt.quiz.websocket;
 
 import com.sun.security.auth.UserPrincipal;
 import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 import java.security.Principal;
@@ -14,9 +16,6 @@ public class UserHandshakeHandler extends DefaultHandshakeHandler {
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
 
         //Create Session IDs for the users
-        String sessionId = UUID.randomUUID().toString();
-        System.out.println("User connected with websocket-sessionId: " + sessionId);
-
-        return new UserPrincipal(sessionId);
+        return new UserPrincipal(UUID.randomUUID().toString());
     }
 }
