872698ea269745e77421eb9d55bc096333ba4ab9
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-23 00:48:36 +0100

    set the token as principal

--- src/main/java/de/semesterprojekt/quiz/websocket/UserHandshakeHandler.java
+++ src/main/java/de/semesterprojekt/quiz/websocket/UserHandshakeHandler.java
@@ -1,9 +1,10 @@
 package de.semesterprojekt.quiz.websocket;
 
 import com.sun.security.auth.UserPrincipal;
+import de.semesterprojekt.quiz.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 import java.security.Principal;
@@ -15,7 +16,11 @@ public class UserHandshakeHandler extends DefaultHandshakeHandler {
     @Override
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
 
-        //Create Session IDs for the users
-        return new UserPrincipal(UUID.randomUUID().toString());
+        //Get the token from the header
+        String headerString = request.getHeaders().toString();
+        String token = headerString.substring(headerString.indexOf("token:\"") + 7,headerString.indexOf("token:\"") + 186);
+
+        //Set the token as Principal
+        return new UserPrincipal(token);
     }
 }
