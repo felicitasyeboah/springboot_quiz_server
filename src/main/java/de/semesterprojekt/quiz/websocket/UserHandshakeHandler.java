c378ac78b0d026e2d2692883b12e9ffe5f0dfe3a
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-22 18:53:58 +0100

    Added a websocket session ID and private messaging support

---
+++ src/main/java/de/semesterprojekt/quiz/websocket/UserHandshakeHandler.java
@@ -0,0 +1,22 @@
+package de.semesterprojekt.quiz.websocket;
+
+import com.sun.security.auth.UserPrincipal;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
+
+public class UserHandshakeHandler extends DefaultHandshakeHandler {
+
+    @Override
+    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+
+        //Create Session IDs for the users
+        String sessionId = UUID.randomUUID().toString();
+        System.out.println("User with ID '" + sessionId + "' connected");
+
+        return new UserPrincipal(sessionId);
+    }
+}
