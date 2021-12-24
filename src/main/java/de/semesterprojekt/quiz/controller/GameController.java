5417c308c12ad29a94ed2f50eb561a5872203f44
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-24 11:37:11 +0100

    set all user's isReady status to false

--- src/main/java/de/semesterprojekt/quiz/controller/GameController.java
+++ src/main/java/de/semesterprojekt/quiz/controller/GameController.java
@@ -3,11 +3,14 @@ package de.semesterprojekt.quiz.controller;
 import de.semesterprojekt.quiz.model.Game;
 import de.semesterprojekt.quiz.model.GameMessage;
 import de.semesterprojekt.quiz.entity.User;
+import de.semesterprojekt.quiz.repository.UserRepository;
 import de.semesterprojekt.quiz.utility.GameFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * The class controls the game
@@ -18,6 +21,9 @@ public class GameController implements ApplicationListener<ContextRefreshedEvent
     @Autowired
     private GameFactory gamefactory;
 
+    @Autowired
+    UserRepository userRepository;
+
     /**
      * The method is called when the server is loaded up
      * @param event
@@ -25,27 +31,13 @@ public class GameController implements ApplicationListener<ContextRefreshedEvent
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
-        //TEST DATA
-        //User user1 = new User();
-        //user1.setUserName("Bernd");
-
-        //User user2 = new User();
-        //user2.setUserName("Beate");
-
-        //Game newGame = gamefactory.createGame(user1, user2);
-
-        //Get all questions of the game
-        //System.out.println(newGame.getQuestionCount());
-        //System.out.println(newGame.getQuestion(0).getQuestionText());
-        //System.out.println(newGame.getQuestion(1).getQuestionText());
-        //System.out.println(newGame.getQuestion(2).getQuestionText());
-
-
-        //Get the gamemessage for user 1 for the first round -> print all answers
-        //GameMessage newGameMessage = newGame.getGameMessage(user1, 0);
-
-        //for(String item : newGameMessage.getAnswer()) {
-        //    System.out.println(item);
-        //}
+        //Set every user's isReady-status to false
+        List<User> userList = userRepository.findAll();
+        for(User user : userList) {
+            if(user.isReady() == true) {
+                user.setReady(false);
+                userRepository.save(user);
+            }
+        }
     }
 }
