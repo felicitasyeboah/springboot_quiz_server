3fd8ba5906da042e0c55ffdc61ee2adf6c9473eb
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-24 23:14:22 +0100

    added a game thread class

---
+++ src/main/java/de/semesterprojekt/quiz/gamelogic/GameThread.java
@@ -0,0 +1,41 @@
+package de.semesterprojekt.quiz.gamelogic;
+
+import com.google.gson.Gson;
+import de.semesterprojekt.quiz.model.Game;
+
+/**
+ * The class implements the game logic
+ */
+public class GameThread implements Runnable{
+
+    private Game game;
+    private WebsocketMessageSender messageSender;
+    Gson gson = new Gson();
+
+    public GameThread(Game game, WebsocketMessageSender messageSender){
+        this.game = game;
+        this.messageSender = messageSender;
+    }
+
+    /**
+     * The method implements the game logic
+     */
+    public void run() {
+
+        //Print the usernames
+        System.out.println("Start game: " + game.getUser1().getUserName() + " vs. " + game.getUser2().getUserName());
+
+        //2 seconds timeout
+        try {
+            System.out.println("Wait two seconds...");
+            Thread.sleep(2000);
+        } catch (Exception e) {
+
+        }
+
+        //Send the first GameMessage to both users
+        System.out.println("First question: " + game.getQuestion(0).getQuestionText());
+        messageSender.sendMessage(game.getTokenUser1(),gson.toJson(game.getGameMessage(game.getUser1(),0)));
+        messageSender.sendMessage(game.getTokenUser2(),gson.toJson(game.getGameMessage(game.getUser2(),0)));
+    }
+}
