a9e980e58a6c8c59ca306c1873f3373bc20888a7
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-24 11:40:20 +0100

    -send the test GameMessage with the calling user as user1
    -added a 5s timer for testing the "lobby"
    
    -set the isReady status to true (websocket connect)
    
    -set the isReady status to false (websocket disconnect)

--- src/main/java/de/semesterprojekt/quiz/websocket/WebSocketController.java
+++ src/main/java/de/semesterprojekt/quiz/websocket/WebSocketController.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import de.semesterprojekt.quiz.model.Game;
 import de.semesterprojekt.quiz.model.GameMessage;
 import de.semesterprojekt.quiz.entity.User;
+import de.semesterprojekt.quiz.repository.UserRepository;
 import de.semesterprojekt.quiz.security.JwtTokenProvider;
 import de.semesterprojekt.quiz.utility.GameFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 public class WebSocketController {
@@ -29,6 +31,9 @@ public class WebSocketController {
     @Autowired
     JwtTokenProvider tokenProvider;
 
+    @Autowired
+    UserRepository userRepository;
+
     @MessageMapping("/game")
     public void getGameMessage(String message, Principal principal){
 
@@ -39,14 +44,38 @@ public class WebSocketController {
         System.out.println("Message from: " + username);
         System.out.println("Token: " + sessionToken);
         System.out.println("Message: " + message);
-        System.out.println("Send the following data to " + username + "\n");
 
-        String testGameMessage = getTestGameMessage();
+        //5s Timer
+        int delay = 5;
+        Thread sendMessage = new Thread() {
+            @Override
+            public void run(){
+                try {
 
-        System.out.println(testGameMessage);
+                    //Kannst du auskommentieren :)
+                    for(int i = delay; i>0 ; i--) {
+                        System.out.println("Wait for another player... " + i + "s left.");
+                        sleep(1000);
+                    }
 
-        //Send Data to user
-        template.convertAndSendToUser(sessionToken , "/topic/game", testGameMessage);
+                    System.out.println("Send the following data to " + username + "\n");
+
+                    //Get the User and create the test GameMessage
+                    Optional<User> userOptional = userRepository.findByUserName(username);
+                    User user = (User) userOptional.get();
+                    String testGameMessage = getTestGameMessage(user);
+
+                    //Print the data
+                    System.out.println(testGameMessage);
+
+                    //Send Data to user
+                    template.convertAndSendToUser(sessionToken , "/topic/game", testGameMessage);
+
+                } catch (Exception e) {
+                }
+            }
+        };
+        sendMessage.run();
     }
 
     /**
@@ -56,8 +85,17 @@ public class WebSocketController {
     @EventListener
     public void handleWebSocketConnectListener(SessionSubscribeEvent event) {
 
-        System.out.println("User \"" + tokenProvider.getUserNameFromToken(event.getUser().toString()) + "\" connected.");
+        //Get the user
+        User user = getUserFromUsername(tokenProvider.getUserNameFromToken(event.getUser().toString()));
 
+        //Set isReady status to true
+        user.setReady(true);
+
+        //Store the object
+        userRepository.save(user);
+
+        //Print the message
+        System.out.println("User \"" + user.getUserName() + "\" connected. Changed isReady to: " + user.isReady());
     }
 
     /**
@@ -67,19 +105,36 @@ public class WebSocketController {
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
 
-        System.out.println("User \"" + tokenProvider.getUserNameFromToken(event.getUser().toString()) + "\" disconnected.");
+        //Get the user
+        User user = getUserFromUsername(tokenProvider.getUserNameFromToken(event.getUser().toString()));
 
+        //Set isReady status to true
+        user.setReady(false);
+
+        //Store the object
+        userRepository.save(user);
+
+        //Print the message
+        System.out.println("User \"" + user.getUserName() + "\" disconnected. Changed isReady to: " + user.isReady());
+
+    }
+
+    /**
+     *
+     */
+    private User getUserFromUsername(String username) {
+        return (User) userRepository.findByUserName(username).get();
     }
 
     /**
      * The method returns a test object
      * @return test object
      */
-    private String getTestGameMessage() {
+    private String getTestGameMessage(User user1) {
 
         //TEST DATA
-        User user1 = new User();
-        user1.setUserName("Bernd");
+        //User user1 = new User();
+        //user1.setUserName("Bernd");
 
         User user2 = new User();
         user2.setUserName("Beate");
