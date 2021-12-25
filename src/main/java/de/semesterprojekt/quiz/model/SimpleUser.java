99840d2ae78a6bb13f45c482ce13a40bcd6b7411
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-25 21:01:30 +0100

    added a simpler form of the user for data transfer without unneeded data

---
+++ src/main/java/de/semesterprojekt/quiz/model/SimpleUser.java
@@ -0,0 +1,19 @@
+package de.semesterprojekt.quiz.model;
+
+import de.semesterprojekt.quiz.entity.User;
+import lombok.Data;
+
+/**
+ * The class represents a simpler form of the user class
+ */
+@Data
+public class SimpleUser {
+
+    private String userName;
+    private String profileImage;
+
+    public SimpleUser(User user) {
+        this.userName = user.getUserName();
+        this.profileImage = user.getProfileImage();
+    }
+}
