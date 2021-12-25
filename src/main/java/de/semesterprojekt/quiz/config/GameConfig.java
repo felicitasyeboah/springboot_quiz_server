52793b794e5688a7bcd6c5947e807bc92d85642b
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-25 21:00:07 +0100

    added a configfile for global constants

---
+++ src/main/java/de/semesterprojekt/quiz/config/GameConfig.java
@@ -0,0 +1,13 @@
+package de.semesterprojekt.quiz.config;
+
+public class GameConfig {
+
+    //The count of the questions
+    public final static int QUESTION_COUNT = 3;
+
+    //The question duration in seconds
+    public final static int QUESTION_DURATION = 10;
+
+    //The time before each question
+    public final static int BREAK_DURATION = 3;
+}
