aeeb476a2dd0481cf6066dedf50c947c0534ebc9
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-26 23:28:16 +0100

    added the break and game timer. The game does now loop!

--- src/main/java/de/semesterprojekt/quiz/game/controller/GameThread.java
+++ src/main/java/de/semesterprojekt/quiz/game/controller/GameThread.java
@@ -26,105 +26,110 @@ public class GameThread extends Thread implements Observer {
     /**
      * The method implements the game logic
      */
+    @Override
     public void run() {
 
-        //Add messageQueue-observer
-        game.addObserver(this);
+        for(int currentRound = 0; currentRound < GameConfig.COUNT_QUESTION; currentRound++)
+        {
+            //Print the usernames
+            System.out.println("Start game: " + game.getUser1().getUserName() + " vs. " + game.getUser2().getUserName());
 
-        //Print the usernames
-        System.out.println("Start game: " + game.getUser1().getUserName() + " vs. " + game.getUser2().getUserName());
-
-        //BREAK_DURATION
-        try {
-            System.out.println("Wait " + GameConfig.DURATION_BREAK + "millis");
-            Thread.sleep(GameConfig.DURATION_BREAK);
-        } catch (Exception e) {
-
-        }
-
-
-
-
-
-        int currentRound = 0;
-
-        //Send the first GameMessage to both users
-        System.out.println("First question: " + game.getQuestion(0).getQuestionText());
-        messageSender.sendMessage(game.getTokenUser1(),gson.toJson(game.getGameMessage(game.getUser1(),currentRound)));
-        messageSender.sendMessage(game.getTokenUser2(),gson.toJson(game.getGameMessage(game.getUser2(),currentRound)));
-
-        //Delete messages from the queue
-        game.clearMessages();
-
-        //Start a timer
-        long startTimeMillis = System.currentTimeMillis();
-        long remainingTimeMillis = GameConfig.DURATION_QUESTION;
-
-        //Has a user answered the question?
-        boolean user1Answered = false;
-        boolean user2Answered = false;
-
-        //How much time has passed to answer
-        long user1Time = 10000;
-        long user2Time = 10000;
-
-        //answers of the users
-        String user1Answer = "";
-        String user2Answer = "";
-
-        //Start the timer loop
-        do {
+            //Break timer
             try {
+                for (int i = GameConfig.DURATION_BREAK; i > 0; i--) {
+                    System.out.println("Break timer: " + i + " seconds left.");
 
-                //Wait the remaining time
-                Thread.sleep(remainingTimeMillis);
+                    //TODO: SEND TIMER MESSAGE
 
-                //Set it to 0 afterwards
-                remainingTimeMillis = 0;
-
-            } catch (InterruptedException e) {
-
-                //Check if theres a message
-                if(game.isNextMessage()) {
-                    IncomingWebSocketMessage message = game.getNextMessage();
-
-                    //Set the used time and answer for the first user
-                    if(game.getUser1().equals(message.getUser()) && !user1Answered) {
-                        user1Time = System.currentTimeMillis() - startTimeMillis;
-                        user1Answer = message.getMessage();
-                        user1Answered = true;
-                        System.out.println(game.getUser1().getUserName() + " has chosen an answer.");
-                    }
-
-                    //Set the used time and answer for the second user
-                    if(game.getUser2().equals(message.getUser()) && !user2Answered) {
-                        user2Time = System.currentTimeMillis() - startTimeMillis;
-                        user2Answer = message.getMessage();
-                        user2Answered = true;
-                        System.out.println(game.getUser2().getUserName() + " has chosen an answer.");
-                    }
+                    Thread.sleep(1000);
                 }
+            } catch (Exception e) {
 
-                //Calculate the new remaining time
-                remainingTimeMillis = GameConfig.DURATION_QUESTION - (System.currentTimeMillis() - startTimeMillis);
-
-                //When both players answered, the remaining time will be set to 0
-                if(user1Answered && user2Answered)
-                {
-                    remainingTimeMillis = 0;
-                }
             }
-        } while (remainingTimeMillis > 0);
 
-        //Print timer-is-over message
-        System.out.println("First round is over");
+            //Print the question of the current round
+            System.out.println("Question " + (currentRound + 1) + ": " + game.getQuestion(currentRound).getQuestionText());
 
-        //Print the correct answer
-        System.out.println("The correct answer is: " + game.getQuestion(currentRound).getAnswerCorrect());
+            //Send the question to both users
+            messageSender.sendMessage(game.getTokenUser1(), gson.toJson(game.getGameMessage(game.getUser1(), currentRound)));
+            messageSender.sendMessage(game.getTokenUser2(), gson.toJson(game.getGameMessage(game.getUser2(), currentRound)));
 
-        //Analysis of the result
-        System.out.println(game.getUser1().getUserName() + " answered: " + user1Answer + " in " + user1Time + " millis. Points: " + calculatePoints(user1Time));
-        System.out.println(game.getUser2().getUserName() + " answered: " + user2Answer + " in " + user2Time + " millis. Points: " + calculatePoints(user2Time));
+            //Delete messages from the queue
+            game.clearMessages();
+
+            //Add messageQueue-observer
+            game.addObserver(this);
+
+            //Start a timer
+            long startTimeMillis = System.currentTimeMillis();
+            long remainingTimeMillis = GameConfig.DURATION_QUESTION * 1000;
+
+            //Has a user answered the question?
+            boolean hasAnsweredUser1 = false;
+            boolean hasAnsweredUser2 = false;
+
+            //Start the client timer
+            GameTimer clientTimer = new GameTimer(this.game, this.messageSender);
+            clientTimer.start();
+
+            //Start the timer loop
+            do {
+                try {
+
+                    //Wait the remaining time
+                    Thread.sleep(remainingTimeMillis);
+
+                    //Set it to 0 afterwards
+                    remainingTimeMillis = 0;
+
+                } catch (InterruptedException e) {
+
+                    //Check if there's a message
+                    if (game.isNextMessage()) {
+                        IncomingWebSocketMessage message = game.getNextMessage();
+
+                        //Submit the answer of user 1
+                        if (game.getUser1().equals(message.getUser()) && !hasAnsweredUser1) {
+
+                            game.submitAnswer(game.getUser1(), currentRound, message.getMessage(), System.currentTimeMillis() - startTimeMillis);
+                            hasAnsweredUser1 = true;
+                            System.out.println(game.getUser1().getUserName() + " has chosen an answer.");
+                        }
+
+                        //Submit the answer of user 2
+                        if (game.getUser2().equals(message.getUser()) && !hasAnsweredUser2) {
+
+                            game.submitAnswer(game.getUser2(), currentRound, message.getMessage(), System.currentTimeMillis() - startTimeMillis);
+                            hasAnsweredUser2 = true;
+                            System.out.println(game.getUser2().getUserName() + " has chosen an answer.");
+                        }
+                    }
+
+                    //Calculate the new remaining time
+                    remainingTimeMillis = GameConfig.DURATION_QUESTION * 1000 - (System.currentTimeMillis() - startTimeMillis);
+
+                    //When both players answered, the remaining time will be set to 0
+                    if (hasAnsweredUser1 && hasAnsweredUser2) {
+                        remainingTimeMillis = 0;
+                    }
+                }
+            } while (remainingTimeMillis > 0);
+
+            //Interrupt the Client Timer
+            clientTimer.interrupt();
+
+            //Remove messageQueue-observer
+            game.deleteObserver(this);
+
+            //TODO: Send a scoreMessage to the users
+
+            //Print timer-is-over message
+            System.out.println("First round is over.");
+
+            //Print the points of the users
+            System.out.println(game.getUser1().getUserName() + " got " + game.getScoreUser1() + " points.");
+            System.out.println(game.getUser2().getUserName() + " got " + game.getScoreUser2() + " points.");
+        }
 
         //Print message
         System.out.println("Please restart the server for further testing");
@@ -145,32 +150,5 @@ public class GameThread extends Thread implements Observer {
         this.interrupt();
     }
 
-    /**
-     * Calculate the points for the user
-     * timeMillis = 0 - GameConfig.MAXPOINTS_DURATION -> GameConfig.POINTS_MAX
-     * timeMillis = GameConfig.MAXPOINTS_DURATION - GameConfig.QUESTION_DURATION -> GameConfig.POINTS_MIN - GameConfig.POINTS_MAX
-     * timeMillis > GameConfig.MAXPOINTS_DURATION -> 0 points
-     *
-     * @param timeMillis needed time to answer
-     * @return
-     */
-    private int calculatePoints (long timeMillis) {
-        if (timeMillis < GameConfig.DURATION_MAX_POINTS) {
 
-            //Return max points
-            return GameConfig.POINTS_MAX;
-
-        } else if (timeMillis >= GameConfig.DURATION_MAX_POINTS && timeMillis < GameConfig.DURATION_QUESTION) {
-
-            long deltaTimeNeededMillis = timeMillis - GameConfig.DURATION_MAX_POINTS;
-            int deltaPoints = GameConfig.POINTS_MAX - GameConfig.POINTS_MIN;
-            long deltaTimeMillis = GameConfig.DURATION_QUESTION - GameConfig.DURATION_MAX_POINTS;
-
-            return GameConfig.POINTS_MIN;
-            //return (int) GameConfig.POINTS_MIN + GameConfig.POINTS_MAX - ((deltaTimeNeededMillis / deltaTimeMillis) * deltaPoints));
-        }
-
-        //Return 0 points
-        return 0;
-    }
 }
