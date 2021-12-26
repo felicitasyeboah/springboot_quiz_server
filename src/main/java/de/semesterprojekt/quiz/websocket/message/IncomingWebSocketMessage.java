1764750e7640890abeb536f82cf5627ede411e4a
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-26 23:27:45 +0100

    added a try catch block

--- src/main/java/de/semesterprojekt/quiz/websocket/message/IncomingWebSocketMessage.java
+++ src/main/java/de/semesterprojekt/quiz/websocket/message/IncomingWebSocketMessage.java
@@ -22,7 +22,10 @@ public class IncomingWebSocketMessage {
         this.user = user;
 
         //Get the Data with key "answer" -> if the key is no present, the field will be an empty string
-        this.message = new Gson().fromJson(message, AnswerMessage.class).getAnswer();
+        try {
+            this.message = new Gson().fromJson(message, AnswerMessage.class).getAnswer();
+        } catch (Exception e) {
+            this.message = "";
+        }
     }
-
 }
