1fe47aa75e900de94e47b123bd569307aa6c3609
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-26 02:57:36 +0100

    changed the values to long type and millisecond values

--- src/main/java/de/semesterprojekt/quiz/config/GameConfig.java
+++ src/main/java/de/semesterprojekt/quiz/config/GameConfig.java
@@ -5,9 +5,9 @@ public class GameConfig {
     //The count of the questions
     public final static int QUESTION_COUNT = 3;
 
-    //The question duration in seconds
-    public final static int QUESTION_DURATION = 10;
+    //The question duration in milliseconds
+    public final static long QUESTION_DURATION = 10000;
 
     //The time before each question
-    public final static int BREAK_DURATION = 3;
+    public final static long BREAK_DURATION = 2000;
 }
