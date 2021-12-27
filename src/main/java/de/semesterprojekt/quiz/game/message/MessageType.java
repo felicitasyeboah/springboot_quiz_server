4d4c53e8ad6335ef8fd4180c1f13717cee536727
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-27 12:24:33 +0100

    added comments

--- src/main/java/de/semesterprojekt/quiz/game/message/MessageType.java
+++ src/main/java/de/semesterprojekt/quiz/game/message/MessageType.java
@@ -4,6 +4,8 @@ package de.semesterprojekt.quiz.game.message;
  * defines the types of the messages a game sends
  */
 public enum MessageType {
+
+    //Add values to the ENUM
     GENERIC_MESSAGE("GENERIC_MESSAGE"),
     GAME_MESSAGE("GAME_MESSAGE"),
     SCORE_MESSAGE("SCORE_MESSAGE"),
@@ -11,12 +13,16 @@ public enum MessageType {
     TIMER_MESSAGE("TIMER_MESSAGE");
 
     private final String value;
+
     private MessageType(String value) {
+
         this.value = value;
     }
 
+    //Override toString method to get the value of the enum
     @Override
     public String toString() {
+
         return this.value;
     }
 }
