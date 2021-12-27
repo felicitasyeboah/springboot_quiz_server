c5a316e16bae23f88275e915a49f6b2ca4f3f907
Christian <christian.kobialka@stud.th-luebeck.de>
2021-12-27 12:25:02 +0100

    get the messagetype from the message object

--- src/main/java/de/semesterprojekt/quiz/websocket/WebsocketMessageSender.java
+++ src/main/java/de/semesterprojekt/quiz/websocket/WebsocketMessageSender.java
@@ -1,5 +1,7 @@
 package de.semesterprojekt.quiz.websocket;
 
+import com.google.gson.Gson;
+import de.semesterprojekt.quiz.game.message.GenericMessage;
 import de.semesterprojekt.quiz.game.message.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.MessageHeaders;
@@ -20,20 +22,27 @@ public class WebsocketMessageSender {
     @Autowired
     SimpMessagingTemplate template;
 
+    Gson gson = new Gson();
+
     /**
      * The method sends a websocket message to a specific user (by token)
      * @param token token of the user
      * @param message message
      */
-    public void sendMessage(String token, String message, MessageType messageType) {
+    public void sendMessage(String token, GenericMessage message) {
 
+        //Create a map and add the message-type header
         Map<String, List<String>> nativeHeaders = new HashMap<>();
-        nativeHeaders.put("type", Collections.singletonList(messageType.toString()));
+        nativeHeaders.put("type", Collections.singletonList(message.getType().toString()));
 
+        //Create a header
         Map<String, Object> headers = new HashMap<>();
         headers.put(NativeMessageHeaderAccessor.NATIVE_HEADERS, nativeHeaders);
 
+        //Print send
+        System.out.println("type: \"" + message.getType().toString() + "\", message: " + gson.toJson(message));
+
         //Send message to user
-        template.convertAndSendToUser(token , "/topic/game", message, headers);
+        template.convertAndSendToUser(token , "/topic/game", gson.toJson(message), headers);
     }
 }
