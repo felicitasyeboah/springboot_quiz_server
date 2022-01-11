81f850390acf2ff172d60e3a22ac28eb64408ce4
Christian <christian.kobialka@stud.th-luebeck.de>
2022-01-11 17:23:47 +0100

    Refactoring and returning the own user with /user

--- src/main/java/de/semesterprojekt/quiz/database/controller/UserController.java
+++ src/main/java/de/semesterprojekt/quiz/database/controller/UserController.java
@@ -2,16 +2,17 @@ package de.semesterprojekt.quiz.database.controller;
 
 import de.semesterprojekt.quiz.database.entity.User;
 import de.semesterprojekt.quiz.database.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import java.util.Optional;
 
 /**
  * The class controls the REST-mapping for the User-entity
  */
 @CrossOrigin("http://localhost:8080")
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
     private UserRepository userRepository;
@@ -26,14 +27,55 @@ public class UserController {
     }
 
     /**
-     * Returns a list of all users
+     * Returns the user object of the calling user
      * @return All users
      */
-    @GetMapping("")
-    public List<User> index(){
+    @GetMapping(path = "/user")
+    public User index(){
 
-        //TODO: return own user object
+        //Get the user from the securityContext
+        return getUserFromSecurityContext(SecurityContextHolder.getContext());
+    }
 
-        return userRepository.findAll();
+    /**
+     * The method returns the user by the username
+     */
+    public User getUserFromUserName(String userName) {
+
+        //Get the user
+        Optional<User> userOptional = userRepository.findByUserName(userName);
+        if(userOptional.isPresent()) {
+            return userOptional.get();
+        }
+
+        //No user found
+        return null;
+    }
+
+    /**
+     * The method returns the user of the securityContext
+     * @param securityContext SecurityContext
+     * @return User
+     */
+    public User getUserFromSecurityContext(SecurityContext securityContext) {
+
+        String userName;
+
+        //Get principal from context
+        Object principal = securityContext
+                .getAuthentication()
+                .getPrincipal();
+
+        //Set the username
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal)
+                    .getUsername();
+        } else {
+            userName = principal
+                    .toString();
+        }
+
+        //Return the user
+        return getUserFromUserName(userName);
     }
 }
