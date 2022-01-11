dec10f93662423887e4ba93591ada6edd6430a1f
Christian <christian.kobialka@stud.th-luebeck.de>
2022-01-11 21:22:19 +0100

    Deleted the random function

--- src/main/java/de/semesterprojekt/quiz/database/controller/QuestionController.java
+++ src/main/java/de/semesterprojekt/quiz/database/controller/QuestionController.java
@@ -3,21 +3,14 @@ package de.semesterprojekt.quiz.database.controller;
 import de.semesterprojekt.quiz.config.GameConfig;
 import de.semesterprojekt.quiz.database.entity.Question;
 import de.semesterprojekt.quiz.database.repository.QuestionRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 /**
- * The class controls the REST-mapping for the Question-entity
+ * The class controls the methods for the Question-entity
  */
-
-@CrossOrigin
-@RestController
-@RequestMapping("/question")
+@Controller
 public class QuestionController {
 
     private QuestionRepository questionRepository;
@@ -31,31 +24,9 @@ public class QuestionController {
      * Returns a list of all questions
      * @return List of questions
      */
-    @GetMapping("/all")
-    public List<Question> index(){
+    private List<Question> index(){
 
         return questionRepository.findAll();
-    }
-
-    /**
-     * Returns a random question
-     * @return question
-     */
-
-    @GetMapping("/random")
-    public Question getRandom() {
-
-        //Create a new random instance
-        Random random = new Random();
-
-        //Get all questions
-        List<Question> questionList = index();
-
-        //Generate a random index
-        int randomNumber = random.nextInt(questionList.size());
-
-        //Return the random Question object
-        return questionList.get(randomNumber);
     }
 
     /**
@@ -65,7 +36,7 @@ public class QuestionController {
     public List<Question> getQuestions() {
 
         //Get a list of all available questions
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = index();
 
         //Shuffle all questions
         Collections.shuffle(questionList);
