1ec1efc6951bd63a153a72b3936dd87d02e45385
Christian <christian.kobialka@stud.th-luebeck.de>
2022-01-31 11:39:06 +0100

    removed the profile image from the simpleuser

--- src/main/java/de/semesterprojekt/quiz/database/model/SimpleUser.java
+++ src/main/java/de/semesterprojekt/quiz/database/model/SimpleUser.java
@@ -3,17 +3,17 @@ package de.semesterprojekt.quiz.database.model;
 import de.semesterprojekt.quiz.database.entity.User;
 import lombok.Data;
 
-/**
+/** TODO: Delete profileImage
  * The class represents a simpler form of the user class
  */
 @Data
 public class SimpleUser {
 
     private String userName;
-    private String profileImage;
+    //private String profileImage;
 
     public SimpleUser(User user) {
         this.userName = user.getUserName();
-        this.profileImage = user.getProfileImage();
+        //this.profileImage = user.getProfileImage();
     }
 }
