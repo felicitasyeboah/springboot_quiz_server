ba2de88ad63fd546eff22caaf7794aa5bfbf33c1
Christian <christian.kobialka@stud.th-luebeck.de>
2022-02-02 17:17:01 +0100

    removed unused sql commands

--- fill_tables.sql
+++ fill_tables.sql
@@ -1,9 +1,3 @@
-INSERT INTO PaF_GruppeB.user (user_name, password)
-VALUES
-    ('user1', 'password1'),
-    ('user2', 'password2'),
-    ('user3', 'password3');
-
 INSERT INTO PaF_GruppeB.category (category_name)
 VALUES
     ('Wissenschaft'),
@@ -28,21 +22,9 @@ VALUES
     (3, 'Welcher der folgenden Künstler ist kein Vertreter des Impressionismus?', 'Andy Warhol', 'Claude Monet', 'Pierre-Auguste Renoir', 'Camille Pissarro'),
     (4, 'Einen Feinschmecker nennt man auch?', 'Gourmet', 'Genießer', 'Gourmed', 'Leckermäulchen'),
     (4, 'Aus welchem Land kommt der Gouda?', 'Niederlande', 'Ghana', 'Frankreich', 'Luxemburg'),
-    (4, 'In welchem land wird am meisten Tee getrunken?', 'England', 'Türkei', 'Kuwait', 'Liechtenstein'),
+    (4, 'In welchem Land wird am meisten Tee getrunken?', 'England', 'Türkei', 'Kuwait', 'Liechtenstein'),
     (4, 'Wie wachsen Erdnüsse?', 'in der Erde', 'an Bäumen', 'wie Erdbeeren am Boden', 'am Busch'),
     (5, 'Wie lange geht ein Marathon?', '42,195 Kilometer', '10 Kilometer', '25 Kilometer', '1000 Meter'),
     (5, 'Aus wie vielen Spieler:innen besteht eine Fußballmannschaft?', '11', '10', '12', '13'),
     (5, 'Wie oft findet eine Fußball-Weltmeisterschaft statt?', 'alle vier Jahre', 'alle zwei Jahre', 'jedes Jahr', 'alle fünf Jahre'),
     (5, 'Was macht der Platzwart von Berufs wegen?','ein paar Linien ziehen','an der Tube schnüffeln','sich einen zwitschern','ne Tüte drehen');
-
-INSERT INTO PaF_GruppeB.played_game (time_stamp, user_id1, user_id2, user_score1, user_score2)
-VALUES
-    ('2021-12-28 11:10:00', 12, 3, 4000, 1450),
-    ('2021-12-28 11:11:00', 1, 2, 3500, 740),
-    ('2021-12-28 11:12:00', 1, 12, 840, 4000),
-    ('2021-12-28 11:13:00', 12, 2, 4000, 1350),
-    ('2021-12-28 11:14:00', 1, 3, 3400, 750),
-    ('2021-12-28 11:15:00', 3, 12, 610, 3100),
-    ('2021-12-28 11:16:00', 12, 2, 3100, 1650),
-    ('2021-12-28 11:17:00', 3, 2, 3450, 770),
-    ('2021-12-28 11:18:00', 2, 1, 680, 3400);
